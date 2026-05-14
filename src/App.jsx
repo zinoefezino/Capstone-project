a829fc4299aa0f9532b927840af7d48e4d080234
Feyisayo29 <adedaboyosayo@gmail.com>
2026-05-14 13:37:33 +0100

    HERO AND VIDEO SECTION

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Hero from './components/Hero';
+import VideoSection from './components/VideoSection';
+
+function App() {
+  return (
+    <div>
+      <Hero />;
+      <VideoSection />
+    </div>
+  );
+}
+
+export default App;
