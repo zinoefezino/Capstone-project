3f34478473fdebd22c1daf0fbb7eb2c575dbdb4c
Zino Akpoghelie <zinoakpoghelie17@gmail.com>
2026-05-19 08:24:39 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import ContactForm from "./components/ContactForm";
-import React from 'react';
-
-import Hero from './components/Hero';
-import VideoSection from './components/VideoSection';
+import React from "react";
+import Hero from "./components/Hero";
+import VideoSection from "./components/VideoSection";
 
 function App() {
   return (
     <div>
-      <ContactForm />
       <Hero />;
       <VideoSection />
+      <ContactForm />
     </div>
   );
 }
 
-export default App;
 export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,29 @@
-export default function Hero() {
-    return (
-        <section className="hero">
-                <div className="container">
-                    <div className="hero-p">
-                        <h1>Explore Our Solar System Through Data</h1>
-                        <p id="hero-p">Understand the planets not just by name, but by measurable facts. From size and mass to gravity and density, this page breaks down the solar system in a clear, data-driven way.</p>
-                        <div className="hero-button">
-                            <a className="primary-button" href="#image">Explore the Data</a> 
-                            <a className="secondary-button" href="#contact">Contact us</a>
-                        </div>
-                    </div>
-                    <div className="hero-image">
-                        <img src="hero.png" alt="the world globe"/>
-                    </div>
-                    
-                </div>
-                
-            </section>
+import "../styles/global.css";
 
-    )
-        
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="container">
+        <div className="hero-p">
+          <h1>Explore Our Solar System Through Data</h1>
+          <p id="hero-p">
+            Understand the planets not just by name, but by measurable facts.
+            From size and mass to gravity and density, this page breaks down the
+            solar system in a clear, data-driven way.
+          </p>
+          <div className="hero-button">
+            <a className="primary-button" href="#image">
+              Explore the Data
+            </a>
+            <a className="secondary-button" href="#contact">
+              Contact us
+            </a>
+          </div>
+        </div>
+        <div className="hero-image">
+          <img src="hero.png" alt="the world globe" />
+        </div>
+      </div>
+    </section>
+  );
 }
